3ca9b4af21341f3450a89f13de58939500ca95e0
umaxcode <maxwellodoom1729@gmail.com>
2024-10-08 17:21:00 +0000

    refactor: updated the security configuration and application properties

--- src/main/java/com/umaxcode/Keycloak/Integration/With/Spring/Boot/config/SecurityConfig.java
+++ src/main/java/com/umaxcode/Keycloak/Integration/With/Spring/Boot/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.umaxcode.Keycloak.Integration.With.Spring.Boot.config;
 
 import com.umaxcode.Keycloak.Integration.With.Spring.Boot.utilities.component.DelegatedEntryPoint;
-import com.umaxcode.Keycloak.Integration.With.Spring.Boot.utilities.component.JwtAuthConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,8 +18,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthConverter jwtAuthConverter;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, DelegatedEntryPoint delegatedEntryPoint) throws Exception {
 
@@ -28,7 +25,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
+                .oauth2ResourceServer(oauth2 -> oauth2.opaqueToken(Customizer.withDefaults()))
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(auth -> auth.authenticationEntryPoint(delegatedEntryPoint));
 
