6cf95023e37d2bf0d50e100475b19838d4ff9c62
umaxcode <maxwellodoom1729@gmail.com>
2024-10-08 07:45:48 +0000

    setup: configure resource server to integrate with keycloak

---
+++ src/main/java/com/umaxcode/Keycloak/Integration/With/Spring/Boot/KeycloakIntegrationWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.umaxcode.Keycloak.Integration.With.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakIntegrationWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakIntegrationWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umaxcode/Keycloak/Integration/With/Spring/Boot/KeycloakIntegrationWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umaxcode.Keycloak.Integration.With.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakIntegrationWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
