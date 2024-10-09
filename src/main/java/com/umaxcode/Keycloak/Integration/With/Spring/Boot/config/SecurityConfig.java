c6fc9ee15e6a4a238dcf1b3750d7a29a5d4df684
umaxcode <maxwellodoom1729@gmail.com>
2024-10-09 14:07:27 +0000

    feat: change the default ROLE_ prefix change to empty string during authorization and customize the default JwtAuthenticationConverter bean

--- src/main/java/com/umaxcode/Keycloak/Integration/With/Spring/Boot/config/SecurityConfig.java
+++ src/main/java/com/umaxcode/Keycloak/Integration/With/Spring/Boot/config/SecurityConfig.java
@@ -1,16 +1,18 @@
 package com.umaxcode.Keycloak.Integration.With.Spring.Boot.config;
 
 import com.umaxcode.Keycloak.Integration.With.Spring.Boot.utilities.component.DelegatedEntryPoint;
-import com.umaxcode.Keycloak.Integration.With.Spring.Boot.utilities.component.JwtAuthConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,8 +21,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthConverter jwtAuthConverter;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, DelegatedEntryPoint delegatedEntryPoint) throws Exception {
 
@@ -28,10 +28,29 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(auth -> auth.authenticationEntryPoint(delegatedEntryPoint));
 
         return http.build();
+    }
+
+
+    @Bean
+    public DefaultMethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler() {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setDefaultRolePrefix("");
+        return expressionHandler;
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix(""); // Default is "SCOPE_"
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles"); // Default is "scope" or "scp"
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 }
