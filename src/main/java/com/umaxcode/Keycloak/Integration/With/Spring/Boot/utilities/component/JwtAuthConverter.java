f1c77d73c934922d2df59a7b1e8afd54ae618429
umaxcode <maxwellodoom1729@gmail.com>
2024-10-09 14:05:05 +0000

    refactor: update custom jwtAuthenticationConverter

--- src/main/java/com/umaxcode/Keycloak/Integration/With/Spring/Boot/utilities/component/JwtAuthConverter.java
+++ src/main/java/com/umaxcode/Keycloak/Integration/With/Spring/Boot/utilities/component/JwtAuthConverter.java
@@ -1,26 +1,20 @@
 package com.umaxcode.Keycloak.Integration.With.Spring.Boot.utilities.component;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.*;
 
 @Component
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
-
-    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
 
     @Value("${application.keycloak.client-id}")
     String clientId;
@@ -28,10 +22,8 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
     @Override
     public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
 
-        Collection<GrantedAuthority> authorities = Stream.concat(jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
-                        extractResourceRoles(jwt).stream())
-                .collect(Collectors.toSet());
-        return new JwtAuthenticationToken(jwt, authorities, getPrincipleClaimName(jwt));
+        Collection<SimpleGrantedAuthority> roles = extractRoles(jwt);
+        return new JwtAuthenticationToken(jwt, roles, getPrincipleClaimName(jwt));
     }
 
     private String getPrincipleClaimName(Jwt jwt) {
@@ -39,26 +31,29 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
         return jwt.getClaim("preferred_username");
     }
 
-    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-        Map<String, Object> resourceAccess;
-        Map<String, Object> resource;
-        Collection<String> resourceRoles;
+    private Collection<SimpleGrantedAuthority> extractRoles(Jwt jwt) {
 
-        if (jwt.getClaim("resource_access") == null) {
-            return Set.of();
+        if (jwt.getClaim("resource_access") != null) {
+            Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+
+            if (resourceAccess.get(clientId) != null) {
+
+                Map<String, Object> applicationAccess = (Map<String, Object>) resourceAccess.get(clientId);
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                List<String> keycloakRoles = objectMapper.convertValue(applicationAccess.get("roles"), new TypeReference<>() {
+                });
+
+                Set<SimpleGrantedAuthority> roles = new HashSet<>();
+
+                for (String keycloakRole : keycloakRoles) {
+                    roles.add(new SimpleGrantedAuthority(keycloakRole));
+                }
+                return roles;
+            }
+
         }
 
-        resourceAccess = jwt.getClaim("resource_access");
-        if (resourceAccess.get(clientId) == null) {
-            return Set.of();
-        }
-
-        resource = (Map<String, Object>) resourceAccess.get(clientId);
-
-        resourceRoles = (Collection<String>) resource.get("roles");
-
-        return resourceRoles.stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                .collect(Collectors.toSet());
+        return Set.of();
     }
 }
